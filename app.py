2b0fbf95774ede09be5aee1e28923e683d9094e7
Srikarreddy224 <132494938+Srikarreddy224@users.noreply.github.com>
2023-05-03 20:20:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,4 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, 'assets/style.css', "https://fonts.googleapis.com/css2?family=Yeseva+One&family=Bruno+Ace+SC&Barlow+Condensed:wght@500&display=swap"], external_scripts=['assets/main.js'])
 
-
-server = app.server
 app.config.suppress_callback_exceptions = True
